5d848ab26f15a7e46c6c3a9ec4b732bebb2e3f1e
sebasMonsalve <sebas.monsalve@gmail.com>
2021-04-13 08:10:34 -0500

    Configurar del swagger, cors, datasourse heroku, log

--- src/main/java/co/udea/hero/api/service/HeroService.java
+++ src/main/java/co/udea/hero/api/service/HeroService.java
@@ -1,13 +1,21 @@
 package co.udea.hero.api.service;
 
+import co.udea.hero.api.controller.HeroController;
 import co.udea.hero.api.model.Hero;
 import co.udea.hero.api.repository.HeroRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class HeroService {
+
+    private final Logger log = LoggerFactory.getLogger(HeroService.class);
 
     private HeroRepository heroRepository;
 
@@ -17,7 +25,15 @@ public class HeroService {
 
     public Hero getHero(Integer id){
         Optional<Hero> optionalHero = heroRepository.findById(id);
+        if(!optionalHero.isPresent()){
+            log.error("Error en el servicio de getHero() ");
+        }
         return optionalHero.get();
+    }
+
+    public List<Hero> getHeroes(){
+        List<Hero> heroesList = heroRepository.findAll();
+        return heroesList;
     }
 
 }
